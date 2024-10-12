db6f7905e67713f29b502f11040e9c4ebf4fb7bf
Luis <marin.guadarrama@gmail.com>
2024-10-11 18:57:01 -0600

    [REF] marin: account.payment _compute_destination_account_id

--- marin/models/account_payment.py
+++ marin/models/account_payment.py
@@ -1,10 +1,52 @@
 from werkzeug.urls import url_quote_plus
 
-from odoo import _, fields, models
+from odoo import _, api, fields, models
 
 
 class AccountPayment(models.Model):
     _inherit = "account.payment"
 
-    journal_type = fields.Selection(related="journal_id.type", string="Journal type", store=True, readonly=True)
+    journal_type = fields.Selection(
+        related="journal_id.type", string="Journal type", store=True, readonly=True,
+    )
     cash_transfer_pos_id = fields.Many2one("pos.session")
+
+    def _get_destination_accounts(self):
+        return {
+            "customer": 
+                self.partner_id.with_company(self.company_id).property_account_receivable_id
+                or self.journal_id.default_receivable_account_id,
+            "supplier":
+                self.partner_id.with_company(self.company_id).property_account_payable_id
+                or self.journal_id.default_payable_account_id,
+        }
+
+    @api.depends("company_id", "journal_id", "partner_id", "partner_type")
+    def _compute_destination_account_id(self):
+        self.destination_account_id = False
+        for pay in self:
+            accounts = self._get_destination_accounts()
+            if pay.partner_type == "customer":
+                pay.destination_account_id = (
+                    accounts.get("customer")
+                    or self.env["account.account"].with_company(pay.company_id).search(
+                        [
+                            *self.env["account.account"]._check_company_domain(pay.company_id),
+                            ("deprecated", "=", False),
+                            ("account_type", "=", "asset_receivable"),
+                        ],
+                        limit=1
+                    )
+                )
+            elif pay.partner_type == "supplier":
+                pay.destination_account_id = (
+                    accounts.get("supplier")
+                    or self.env["account.account"].with_company(pay.company_id).search(
+                        [
+                            *self.env["account.account"]._check_company_domain(pay.company_id),
+                            ("deprecated", "=", False),
+                            ("account_type", "=", "liability_payable"),
+                        ],
+                        limit=1
+                    )
+                )
