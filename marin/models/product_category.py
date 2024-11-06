f92e276b1ca1faade6bf44b086e949356f7fbfc2
Luis <marin.guadarrama@gmail.com>
2024-11-06 03:25:04 -0600

    [REF] marin: code styling in product_category.py

--- marin/models/product_category.py
+++ marin/models/product_category.py
@@ -9,33 +9,35 @@ class ProductCategoryInherit(models.Model):
         "Income Refund Account",
         company_dependent=True,
         domain=[("deprecated", "=", False)],
-        help="Used as default value on the customer credit notes lines. " "Leave empty to use the income account.",
+        help="Used as default value on the customer credit notes lines. "
+             "Leave empty to use the income account.",
     )
     property_account_expense_refund_id = fields.Many2one(
         "account.account",
         "Expense Refund Account",
         company_dependent=True,
         domain=[("deprecated", "=", False)],
-        help="Used as default value on the vendor refunds lines. " "Leave empty to use the expense account.",
+        help="Used as default value on the vendor refunds lines. "
+             "Leave empty to use the expense account.",
     )
     expiration_time = fields.Integer(
         string="Expiration Date",
-        help="Number of days after the receipt of the products (from the vendor"
-        " or in stock after production) after which the goods may become dangerous"
-        " and must not be consumed. It will be computed on the lot/serial number.",
+        help="Number of days after the receipt of the products (from the vendor "
+             "or in stock after production) after which the goods may become dangerous "
+             "and must not be consumed. It will be computed on the lot/serial number.",
     )
     use_time = fields.Integer(
         string="Best Before Date",
-        help="Number of days before the Expiration Date after which the goods starts"
-        " deteriorating, without being dangerous yet. It will be computed on the lot/serial number.",
+        help="Number of days before the Expiration Date after which the goods starts to "
+             "deteriorate. It will be computed on the lot/serial number.",
     )
     removal_time = fields.Integer(
         string="Removal Date",
-        help="Number of days before the Expiration Date after which the goods"
-        " should be removed from the stock. It will be computed on the lot/serial number.",
+        help="Number of days before the Expiration Date after which the goods "
+             "should be removed from the stock. It will be computed on the lot/serial number.",
     )
     alert_time = fields.Integer(
         string="Alert Date",
-        help="Number of days before the Expiration Date after which an alert should be"
-        " raised on the lot/serial number. It will be computed on the lot/serial number.",
+        help="Number of days before the Expiration Date after which an alert should be "
+             "raised on the lot/serial number. It will be computed on the lot/serial number.",
     )
