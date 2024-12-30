4f51a2afc517b82dd6ee147d493ac9d46641485a
Luis <marin.guadarrama@gmail.com>
2024-12-30 10:08:54 -0600

    [REF] marin: code clean up due to feature implemented in core

--- marin/models/stock_move_line.py
+++ marin/models/stock_move_line.py
@@ -17,12 +17,6 @@ class StockMoveLineInherit(models.Model):
         compute="_compute_location_availability",
         readonly=True
     )
-    #TODO It seem odoo 18 will solve this natively, delete if so
-    location_lot_domain = fields.Binary(
-        "Location Lot domain",
-        compute="_compute_location_lot_domain",
-        help="Dynamic domain used for the lot that can be set on move line",
-    )
 
     @api.onchange("product_id", "location_id", "location_dest_id")
     def _compute_location_availability(self):
@@ -36,20 +30,6 @@ class StockMoveLineInherit(models.Model):
                 line.location_dest_availability = self.env["stock.quant"]._get_available_quantity(
                     line.product_id, line.location_dest_id, line.lot_id
                 )
-
-    @api.depends("location_id", "product_id")
-    def _compute_location_lot_domain(self):
-        for line in self:
-            domain = [
-                ("company_id", "=", line.company_id.id),
-                ("product_id", "=", line.product_id.id)
-            ]
-            if line.location_usage in ("internal"):
-                quant_lot_ids = line.location_id.quant_ids.filtered(
-                    lambda q: q.quantity > 0 and q.company_id == line.company_id
-                ).mapped("lot_id")
-                domain = [("id", "in", quant_lot_ids.ids)]
-            line.location_lot_domain = domain
 
     # Override original method
     @api.ondelete(at_uninstall=False)
