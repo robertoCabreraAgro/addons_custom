6762a28d1ca4ec9551331602292dab1461d6d9ae
Roberto Cabrera <roberto.cabrera@agromarin.mx>
2025-01-25 10:50:27 -0600

    [WIP] marin hr_payslip comment herency _create_account_move

--- marin/models/hr_payslip.py
+++ marin/models/hr_payslip.py
@@ -59,10 +59,10 @@ class HrPayslipInherit(models.Model):
         payroll["inability_data"] = lambda i, p: p._get_inability_data(i)
         return payroll
 
-    def _create_account_move(self, values):
-        if not values.get("partner_id") and not self.company_id.batch_payroll_move_lines:
-            values["partner_id"] = self.employee_id.work_contact_id.id
-        return super()._create_account_move(values)
+    # def _create_account_move(self, values):
+    #     if not values.get("partner_id") and not self.company_id.batch_payroll_move_lines:
+    #         values["partner_id"] = self.employee_id.work_contact_id.id
+    #     return super()._create_account_move(values)
 
     def _prepare_line_values(self, line, account_id, date, debit, credit):
         res = super()._prepare_line_values(line, account_id, date, debit, credit)
