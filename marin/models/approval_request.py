a381d1c76fe1296b2b39e5e6b307195749850b29
Luis <marin.guadarrama@gmail.com>
2025-02-24 21:10:46 -0600

    [FIX] marin: approvals

--- marin/models/approval_request.py
+++ marin/models/approval_request.py
@@ -18,14 +18,6 @@ class ApprovalRequest(models.Model):
         compute="_compute_vehicle_id", store=True,
         readonly=False,
     )
-    license_plate = fields.Char(
-        string="License Plate",
-        compute="_compute_vehicle_info", store=True,
-    )
-    fuel_type = fields.Char(
-        string="Fuel Type",
-        compute="_compute_vehicle_info", store=True,
-    )
     odometer = fields.Integer(
         string="Odometer",
         help="Enter the vehicle's current odometer reading.",
@@ -47,16 +39,6 @@ class ApprovalRequest(models.Model):
                 record.vehicle_id = vehicle if vehicle else False
             else:
                 record.vehicle_id = False
-
-    @api.depends("vehicle_id")
-    def _compute_vehicle_info(self):
-        for record in self:
-            if record.vehicle_id:
-                record.license_plate = record.vehicle_id.license_plate
-                record.fuel_type = record.vehicle_id.fuel_type
-            else:
-                record.license_plate = False
-                record.fuel_type = False
 
     def action_confirm(self):
         for request in self:
