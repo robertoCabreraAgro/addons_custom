05719dafcea9825f466bca223a705578b2d15335
Luis <marin.guadarrama@gmail.com>
2025-03-09 10:52:28 -0600

    [REF] gps_tracking: styling

--- gps_tracking/__manifest__.py
+++ gps_tracking/__manifest__.py
@@ -1,25 +1,26 @@
+
 {
-    'name': 'GPS Tracking',
-    'version': '1.0',
-    'category': 'Fleet Management',
-    'summary': 'Module to track GPS coordinates and display them on a map',
-    'author': 'Raúl Alejandro Rodríguez López',
-    'website': 'https://raulalejandro.com.mx',
-    'depends': ['fleet'], 
-    'data': [
-        'security/ir.model.access.csv',
-        'views/gps_tracking_device_views.xml',
-        'views/gps_tracking_menus.xml', # Last because referencing actions defined in previous files
+    "name": "GPS Tracking",
+    "version": "1.0",
+    "category": "Fleet Management",
+    "summary": "Module to track GPS coordinates and display them on a map",
+    "author": "Raúl Alejandro Rodríguez López",
+    "website": "https://raulalejandro.com.mx",
+    "depends": ["fleet"], 
+    "data": [
+        "security/ir.model.access.csv",
+        "views/gps_tracking_device_views.xml",
+        "views/gps_tracking_menus.xml", # Last because referencing actions defined in previous files
     ], 
-    'demo': [
-        'demo/gps_tracking_device_data.xml',
-        'demo/gps_tracking_point_data.xml',
+    "demo": [
+        "demo/gps_tracking_device_data.xml",
+        "demo/gps_tracking_point_data.xml",
     ],
-    #'assets': {
-    #    'web.assets_backend': [
-    #    'gps_tracking/static/src/**/*'
+    #"assets": {
+    #    "web.assets_backend": [
+    #    "gps_tracking/static/src/**/*"
     #    ],
     #},
-    'installable': True,
-    'application': True,
+    "installable": True,
+    "application": True,
 }
--- gps_tracking/models/gps_tracking_device.py
+++ gps_tracking/models/gps_tracking_device.py
@@ -10,10 +10,31 @@ class GpsTrackingDevice(models.Model):
     _name = "gps.tracking.device"
     _description = "GPS Tracking Device"
 
+
     imei = fields.Char(
         string="IMEI",
         required=True,
     )
+
+    vehicle_id = fields.Many2one(
+        comodel_name="fleet.vehicle",
+        string="Vehícle,
+        help="Vehículo asociado al dispositivo GPS",
+    )
+    license_plate = fields.Char(
+        related="vehicle_id.license_plate", store=True,
+        string="Plate",
+    )
+    driver_name = fields.Char(
+        string="Driver",
+        compute="_compute_driver_name", store=True,
+    )
+    model_id = fields.Many2one(
+        related="vehicle_id.model_id", store=True,
+        comodel_name="fleet.vehicle.model",
+        string="Model",
+    )
+
     tracking_points = fields.One2many(
         comodel_name="gps.tracking.point",
         inverse_name="device_id",
@@ -43,8 +64,15 @@ class GpsTrackingDevice(models.Model):
         related="last_point_id.address", store=True,
         string="Altitude",
     )
-    # the_point = fields.GeoPoint(string='Current Position', related='last_point_id.the_point', store=True)
-    # history_route = fields.GeoLine(string='History Route', compute='_compute_history_route', store=True, srid=3857,)
+    # the_point = fields.GeoPoint(
+    #   string='Current Position',
+    #   related='last_point_id.the_point', store=True,
+    # )
+    # history_route = fields.GeoLine(
+    #   string='History Route',
+    #   compute='_compute_history_route', store=True,
+    #   srid=3857,
+    # )
     gsm_signal = fields.Integer(
         related="last_point_id.gsm_signal", store=True,
         string="Gsm Signal",
@@ -57,24 +85,7 @@ class GpsTrackingDevice(models.Model):
         related="last_point_id.movement", store=True,
         string="Movement (240)",
     )
-    vehicle_id = fields.Many2one(
-        comodel_name="fleet.vehicle",
-        string="Vehículo",
-        help="Vehículo asociado al dispositivo GPS",
-    )
-    license_plate = fields.Char(
-        related="vehicle_id.license_plate", store=True,
-        string="Placas",
-    )
-    driver_name = fields.Char(
-        string="Conductor",
-        compute="_compute_driver_name", store=True,
-    )
-    model_id = fields.Many2one(
-        comodel_name="fleet.vehicle.model",
-        string="Modelo",
-        related="vehicle_id.model_id", store=True,
-    )
+
     color = fields.Selection(
         selection=[
             ("#FF0000", "Rojo"),
@@ -95,23 +106,23 @@ class GpsTrackingDevice(models.Model):
 
     @api.depends("vehicle_id.driver_id")
     def _compute_driver_name(self):
-        for record in self:
-            record.driver_name = (
-                record.vehicle_id.driver_id.name if record.vehicle_id.driver_id else ""
+        for device in self:
+            device.driver_name = (
+                device.vehicle_id.driver_id.name if device.vehicle_id.driver_id else ""
             )
 
     # @api.depends('tracking_points.timestamp')  # Usar el campo correcto
     # def _compute_last_point(self):
-    #    for rec in self:
+    #    for device in self:
     #        # Obtener el último punto usando tracking_points
-    #        last_point = rec.tracking_points.sorted(key=lambda p: p.timestamp, reverse=True)[:1]
-    #        rec.last_point_id = last_point.id if last_point else False
+    #        last_point = device.tracking_points.sorted(key=lambda p: p.timestamp, reverse=True)[:1]
+    #        device.last_point_id = last_point.id if last_point else False
     #
     #        # Log para depurar el valor del último punto
     #        if last_point:
-    #            _logger.info(f"Dispositivo {rec.id}: Último punto actualizado a {last_point.id} con timestamp {last_point.timestamp}")
+    #            _logger.info(f"Dispositivo {device.id}: Último punto actualizado a {last_point.id} con timestamp {last_point.timestamp}")
     #        else:
-    #            _logger.warning(f"Dispositivo {rec.id}: No se encontraron puntos para actualizar el último punto.")
+    #            _logger.warning(f"Dispositivo {device.id}: No se encontraron puntos para actualizar el último punto.")
 
     # @api.depends('tracking_points.latitude', 'tracking_points.longitude')
     # def _compute_history_route(self):
