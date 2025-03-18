9149a675728b21edefb7506f7faa97d650bc9600
Luis <marin.guadarrama@gmail.com>
2025-03-17 19:24:00 -0600

    [REM] approvals modules

--- approval_account/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import approval_category
-from . import approval_product_line
-from . import approval_request
-from . import account_move
--- approval_purchase/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import approval_category
-from . import approval_product_line
-from . import approval_request
-from . import res_partner
-from . import purchase_order
