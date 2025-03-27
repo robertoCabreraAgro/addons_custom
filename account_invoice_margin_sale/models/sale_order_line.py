0ce6bcd75b7b126e4c9df96b36de228b92697a52
Luis <marin.guadarrama@gmail.com>
2025-03-27 15:37:48 -0600

    [REF] function renaming

--- account_invoice_margin_sale/models/sale_order_line.py
+++ account_invoice_margin_sale/models/sale_order_line.py
@@ -4,7 +4,7 @@ from odoo import models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    def _prepare_invoice_line(self, **optional_values):
-        vals = super()._prepare_invoice_line(**optional_values)
+    def _prepare_aml_from_sol_vals(self, **optional_values):
+        vals = super()._prepare_aml_from_sol_vals(**optional_values)
         vals["purchase_price"] = self.sudo().purchase_price
         return vals
