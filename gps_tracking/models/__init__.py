ea0b7bdaa9406dbd481bdebe0ce007d2f880e483
German Loredo Becerra <glbxd1@gmail.com>
2025-07-11 08:51:28 -0600

    [FIX] gps_tracking: Error when opening pos
    A new field was added in [1] but it must be also in hr employee public
    in order to be accessible from all users and don't give an error when
    opening pos.
    
    [1]: https://github.com/maringuadarrama/addons_custom/commit/d0799707

--- gps_tracking/models/__init__.py
+++ gps_tracking/models/__init__.py
@@ -4,3 +4,4 @@ from . import gps_tracking_device
 from . import gps_tracking_point
 from . import gps_geofence
 from . import hr_employee
+from . import hr_employee_public
---
+++ gps_tracking/models/hr_employee_public.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+class HrEmployeePublic(models.Model):
+    _inherit = 'hr.employee.public'
+
+    enable_vehicle_loan = fields.Boolean(
+        string="Enable Vehicle Loan Report",
+        default=False,
+        help="If checked, this employee's trips will be included in the vehicle loan report."
+    )
