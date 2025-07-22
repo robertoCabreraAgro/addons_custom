2260420d454dee1d791089080df28b85252f8fc3
Jose Suniaga <jose.suniaga@agromarin.mx>
2025-07-22 11:18:35 -0600

    [MIG] product_abc_classification: Migration to 18.0 (#86)
    Task ID: 7361

---
+++ product_abc_classification/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Product Abc Classification",
+    "summary": """
+        ABC classification for sales and warehouse management""",
+    "version": "1.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV, ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "depends": ["product", "stock"],
+    "data": [
+        "views/abc_classification_product_level.xml",
+        "views/abc_classification_profile.xml",
+        "views/product_template.xml",
+        "views/product_product.xml",
+        "views/product_category.xml",
+        "security/ir.model.access.csv",
+        "data/ir_cron.xml",
+    ],
+    "application": False,
+    "installable": True,
+}
