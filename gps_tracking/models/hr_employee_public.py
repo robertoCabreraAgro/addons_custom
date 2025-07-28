1a6b49d235dd6dc3964ae016f1edc0dbe6599d5c
German Loredo Becerra <glbxd1@gmail.com>
2025-07-28 16:50:33 -0600

    [ADD] gps_tracking: server actions for vehicle loan report management
    Add server actions and public view to enable/disable vehicle loan report
    feature for HR employees. Includes bulk operations for multiple employee
    selection and dedicated public interface for HR management.

--- gps_tracking/models/hr_employee_public.py
+++ gps_tracking/models/hr_employee_public.py
@@ -9,3 +9,13 @@ class HrEmployeePublic(models.Model):
         default=False,
         help="If checked, this employee's trips will be included in the vehicle loan report.",
     )
+
+    def action_enable_vehicle_loan_report(self):
+        """Server action to enable vehicle loan report for selected employees."""
+        self.write({'enable_vehicle_loan': True})
+        return True
+
+    def action_disable_vehicle_loan_report(self):
+        """Server action to disable vehicle loan report for selected employees."""
+        self.write({'enable_vehicle_loan': False})
+        return True
