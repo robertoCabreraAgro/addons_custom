b392c04e88b0e25991e1b180b1fbadbe28bc3f97
Jose Suniaga <jose.suniaga@agromarin.mx>
2025-08-20 17:23:59 -0600

    [IMP] project_task_description_template: filter templates by projects
    Filter templates by projects previously configured in the templates
    - project.task domain updated and store=True
    - project.task.description.template added project_ids
    
    Task: 10615

--- project_task_description_template/models/project_task.py
+++ project_task_description_template/models/project_task.py
@@ -8,7 +8,8 @@ class ProjectTask(models.Model):
     _inherit = "project.task"
 
     description_template_id = fields.Many2one(
-        "project.task.description.template", store=False
+        "project.task.description.template", store=True,
+        domain="['|', ('project_ids', '=', False),('project_ids', 'in', (project_id))]"
     )
 
     @api.onchange("description_template_id")
@@ -16,3 +17,9 @@ class ProjectTask(models.Model):
         if self.description_template_id:
             description = self.description if self.description else ""
             self.description = description + self.description_template_id.description
+
+    @api.onchange('project_id')
+    def _onchange_project_id(self):
+        """Method that clears the selected template when switching to a project to which this template is not associated."""
+        if self.project_id and self.description_template_id and self.project_id not in self.description_template_id.project_ids:
+            self.description_template_id = False
