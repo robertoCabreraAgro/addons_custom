e9553630b2bc545ff8a00c19134c68e00c765fb1
JemXiaoLong <escuderojem@outlook.com>
2025-08-22 13:44:55 -0600

    [REF] estate_property: merge menus and update date logic
    - Eliminated estate_menus.xml and merged its content into
      estate_property_views.xml to simplify view management.
    
    - Replaced datetime.today() with fields.Date.today() to
      automatically respect user's timezone.
    
    - Removed 'models': ['estate.property'] from manifest as
      it was unnecessary.

---
+++ estate/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Modulo del JEM',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Module for managing real estate advertising' ,
+    'depends': [
+        'base'
+        ],
+
+'data': [
+    'views/estate_property_views.xml',
+    'security/ir.model.access.csv',
+],
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
+
+
+
---
+++ estate/models/estate_property.py
@@ -0,0 +1,40 @@
+from odoo import fields, models
+from datetime import datetime, timedelta
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Property"
+
+    name = fields.Char(required=True)
+
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(
+        default=lambda self: fields.Date.today() + timedelta(days=90),
+        copy=False
+    )
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(
+        readonly=True,
+        copy=False
+    )
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection([
+        ('north', 'North'),
+        ('south', 'South'),
+        ('east', 'East'),
+        ('west', 'West'),
+    ])
+    active = fields.Boolean(default=True)
+    state = fields.Selection([
+        ('new', 'New'),
+        ('offer_received', 'Offer Received'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('cancelled', 'Cancelled'),
+    ], string="State", default='new', required=True, copy=False)
