c9cbcd10a4afcc8a700208601e0ba31c5fb12670
lumpuy <lumpuy@hotmail.com>
2025-08-28 12:51:56 -0400

    [FIX] planning_mrp: lint:
    - Uniformize single quotes to double quotes
    Task: 10825

--- planning_mrp/models/mrp_workcenter.py
+++ planning_mrp/models/mrp_workcenter.py
@@ -2,16 +2,16 @@ from odoo import fields, models
 
 
 class MrpWorkcenter(models.Model):
-    _inherit = 'mrp.workcenter'
+    _inherit = "mrp.workcenter"
 
     planning_slot_count = fields.Integer(
-        string='Planning Slot Count',
-        compute='_compute_planning_slot_count'
+        string="Planning Slot Count",
+        compute="_compute_planning_slot_count"
     )
     planning_slot_ids = fields.One2many(
-        'planning.slot',
-        'workcenter_id',
-        string='Planning Slots'
+        "planning.slot",
+        "workcenter_id",
+        string="Planning Slots"
     )
 
     def _compute_planning_slot_count(self):
@@ -26,13 +26,13 @@ class MrpWorkcenter(models.Model):
         only the planning slots related to the work center from which the action was called.
         """
         self.ensure_one()
-        action = self.env.ref('planning_mrp.action_planning_slot_by_workcenter').read()[0]
+        action = self.env.ref("planning_mrp.action_planning_slot_by_workcenter").read()[0]
 
-        action['domain'] = [('workcenter_id', '=', self.id)]
+        action["domain"] = [("workcenter_id", "=", self.id)]
 
-        action['context'] = {
-            'default_workcenter_id': self.id,
-            'search_default_workcenter_id': self.id,
+        action["context"] = {
+            "default_workcenter_id": self.id,
+            "search_default_workcenter_id": self.id,
             **self.env.context
         }
         return action
--- planning_mrp/models/planning_slot.py
+++ planning_mrp/models/planning_slot.py
@@ -3,32 +3,32 @@ from odoo.exceptions import ValidationError
 
 
 class PlanningSlot(models.Model):
-    _name = 'planning.slot'
-    _inherit = ['planning.slot', 'mail.thread']
-    _description = 'Planning Slot with Mail Thread'
+    _name = "planning.slot"
+    _inherit = ["planning.slot", "mail.thread"]
+    _description = "Planning Slot with Mail Thread"
 
     state = fields.Selection(selection_add=[
-        ('cancelled', 'Cancelled')
-    ], ondelete={'cancelled': 'set default'})
+        ("cancelled", "Cancelled")
+    ], ondelete={"cancelled": "set default"})
 
     workcenter_id = fields.Many2one(
-        comodel_name='mrp.workcenter',
-        string='Work Center',
+        comodel_name="mrp.workcenter",
+        string="Work Center",
         check_company=True,
         tracking=True
     )
     product_id = fields.Many2one(
-        comodel_name='product.product',
-        string='Product to Manufacture',
-        domain=[('bom_ids', '!=', False)],
+        comodel_name="product.product",
+        string="Product to Manufacture",
+        domain=[("bom_ids", "!=", False)],
         tracking=True
     )
     planning_manufacturing_project_id = fields.Many2one(
-        comodel_name='project.project',
-        string='Default Manufacturing Planning Project'
+        comodel_name="project.project",
+        string="Default Manufacturing Planning Project"
     )
 
-    @api.constrains('workcenter_id', 'resource_id')
+    @api.constrains("workcenter_id", "resource_id")
     def _check_company(self):
         for slot in self:
             if slot.workcenter_id and slot.resource_id and slot.workcenter_id.company_id != slot.resource_id.company_id:
@@ -40,7 +40,7 @@ class PlanningSlot(models.Model):
             and a specific domain.
 
             Args:
-            action_xml_id (str): The XML ID of the action to retrieve (e.g., 'planning_mrp.action_planning_slot_by_workcenter').
+            action_xml_id (str): The XML ID of the action to retrieve (e.g., "planning_mrp.action_planning_slot_by_workcenter").
             domain (list): The domain to apply to the action.
 
             Returns:
@@ -48,49 +48,39 @@ class PlanningSlot(models.Model):
         """
         action = self.env.ref(action_xml_id).read()[0]
 
-        project_id_str = self.env['ir.config_parameter'].sudo().get_param(
-            'planning_mrp.default_planning_manufacturing_project_id'
+        project_id_str = self.env["ir.config_parameter"].sudo().get_param(
+            "planning_mrp.default_planning_manufacturing_project_id"
         )
         project_id = int(project_id_str) if project_id_str else False
 
         if project_id:
-            action['context'] = {'default_project_id': project_id, **self.env.context}
+            action["context"] = {"default_project_id": project_id, **self.env.context}
 
-        action['domain'] = domain
+        action["domain"] = domain
         return action
 
     def get_action_planning_slot_by_resource(self):
-        """
-        Gets the planning action by resource.
-        """
+        """  Gets the planning action by resource. """
         return self._get_planning_action(
-            'planning.planning_action_schedule_by_resource',
-            []
+            "planning.planning_action_schedule_by_resource", []
         )
 
     def get_action_planning_slot_by_role(self):
-        """
-        Get the planning action by prole.
-        """
+        """ Get the planning action by prole. """
         return self._get_planning_action(
-            'planning.planning_action_schedule_by_role',
-            []
+            "planning.planning_action_schedule_by_role", []
         )
 
     def get_action_planning_slot_by_workcenter(self):
-        """
-        Gets the planning action by work center.
-        """
+        """ Gets the planning action by work center. """
         return self._get_planning_action(
-            'planning_mrp.action_planning_slot_by_workcenter',
-            [('workcenter_id', '!=', False)]
+            "planning_mrp.action_planning_slot_by_workcenter",
+            [("workcenter_id", "!=", False)]
         )
 
     def get_action_planning_slot_by_product(self):
-        """
-        Get the planning action by product.
-        """
+        """ Get the planning action by product. """
         return self._get_planning_action(
-            'planning_mrp.action_planning_slot_by_product',
-            [('product_id', '!=', False)]
+            "planning_mrp.action_planning_slot_by_product",
+            [("product_id", "!=", False)]
         )
