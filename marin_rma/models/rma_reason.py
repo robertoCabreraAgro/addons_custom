7cbcdda770bf5a9f16bc533b0d19290a565041df
lumpuy <lumpuy@hotmail.com>
2025-09-12 19:32:17 -0400

    [IMP] marin_rma: add unique constraint to name field in model rma_reason Task: 11440

--- marin_rma/models/rma_reason.py
+++ marin_rma/models/rma_reason.py
@@ -1,11 +1,20 @@
-from odoo import fields, models
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
 
 
 class RmaReason(models.Model):
     _name = "rma.reason"
     _description = "RMA Reason"
 
-    name = fields.Char(string="Name", required=True, unique=True)
+    name = fields.Char(string="Name", required=True)
     description = fields.Text(string="Description")
     sequence = fields.Integer(string="Sequence")
     active = fields.Boolean(string="Active", default=True)
+
+    @api.constrains('name')
+    def _check_name_unique(self):
+        for record in self:
+            domain = [('name', '=', record.name), ('id', '!=', record.id)]
+            existing_records = self.search(domain)
+            if existing_records:
+                raise ValidationError(_('The name “%s” already exists.', record.name))
