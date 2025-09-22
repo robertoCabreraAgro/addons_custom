ca45d9441a73f508fd1a46b631f9c1ccfa7f851f
Luis <marin.guadarrama@gmail.com>
2025-09-21 22:10:40 -0600

    [REF] product_asset: mixins were not approved

--- product_asset/models/mixins/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import asset_mixin_base
-from . import asset_mixin_financial
-from . import asset_mixin_vehicle
-from . import asset_mixin_personnel
-from . import asset_mixin_contract
-from . import asset_mixin_service
